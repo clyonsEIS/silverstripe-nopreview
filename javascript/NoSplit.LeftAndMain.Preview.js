c2a114bc9584168321492531ed5881443621ecdb
Craig Lyons <clyons@execproinc.com>
2014-08-15 12:51:38 -0400

    Replace custom disable w/ built-in disablePreview

--- javascript/NoSplit.LeftAndMain.Preview.js
+++ javascript/NoSplit.LeftAndMain.Preview.js
@@ -1,16 +1,10 @@
 (function($) {
-
 	$.entwine(
-
 		'ss.preview', function($){
 			$('.cms-preview').entwine({
 					DefaultMode: 'content',
 					onmatch: function() {
-						// no split, also not before this. Default to content. 
-						if ( this.loadState('mode') == 'split') {
-							this.changeMode('content', true);
-						}
-						this._super();					
+                        this.disablePreview();
 					}
 																			
 				}
@@ -19,7 +13,3 @@
 		}
 	);
 }(jQuery));
-
-
-
-
